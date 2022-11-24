85600f74e0f813c08cacfb28c1fc964c2a7171a6
Collins Kiragu <collins.kiragu@student.moringaschool.com>
2022-11-24 22:22:41 +0300

    Correction

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+let ride=199;
+function scuberGreetingForFeet(ride){
+  if (ride<=400){
+    return `This one is on me!`;
+  }
+
+  else if (ride>2000 && ride<2500){
+    return 'I will gladly take your thirty bucks.';
+  }
+
+  else{
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+let city;
+function ternaryCheckCity(city){
+  return city==='NYC' ? `Ok, sounds good.` : `No go.`;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+let tip;
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case 'generous':
+      return "Thank you so much.";
+      break;
+    case 'not as generous':
+      return "Thank you.";
+      break;
+    default:
+      return "Bye.";
+  }
 }
